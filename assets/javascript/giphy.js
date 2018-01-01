80db6ac5aac88f7cf58aa72c98c5fade19f11471
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2017-12-31 21:22:59 -0800

    Add new Villian button working
    I am misspelling villain everywhere
    I should correct that...

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,7 +2,7 @@ var villians = ["Annie Wilkes", "Hans Gruber", "Patrick Bateman", "Gollum", "Jok
 
 function renderButtons() {
     
-            // Deletes the movies prior to adding new movies
+            // Deletes the villians prior to adding new movies
             // (this is necessary otherwise you will have repeat buttons)
             $("#villianButtons").empty();
             // Loops through the array of movies
@@ -21,5 +21,18 @@ function renderButtons() {
               $("#villianButtons").append(a);
             }
           }
+
+// This function handles events where the add villian button is clicked
+$("#addVillian").on("click", function(event) {
+    event.preventDefault();
+    // This line of code will grab the input from the textbox
+    var villian = $("#villian-input").val().trim();
+
+    // The villian from the textbox is then added to our array
+    villians.push(villian);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+  });
 
           renderButtons();
